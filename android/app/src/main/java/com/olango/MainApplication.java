6aded118218e3be8c9880ede4675c2ce73b52c1e
phemy <phemy.smith@gmail.com>
2018-01-31 10:51:08 +0100

    working state

--- android/app/src/main/java/com/olango/MainApplication.java
+++ android/app/src/main/java/com/olango/MainApplication.java
@@ -3,7 +3,6 @@ package com.olango;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
 import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
             new ReactNativeYouTube(),
             new ReactVideoPackage()
       );
